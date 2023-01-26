6af190aca13246e365c2aa605b77de93d7900cb4
austin <bomar289@gmail.com>
2023-01-25 18:24:20 -0800

    general outline

--- main/hydro_monitor.c
+++ main/hydro_monitor.c
@@ -1,6 +1,18 @@
 #include <stdio.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "esp32_mock.h"
+
+#define LEVEL_SENSOR_PIN 17
+#define PUMP_PIN 15
+
+static TaskHandle_t checkWaterTask;
+
 
 void app_main(void)
 {
+
+    
+
 
 }
